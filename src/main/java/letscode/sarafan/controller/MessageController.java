f897b1081e12ed28a1ab416c66416b5ac8b9b7b2
youbantwo <youbantwo@gmail.com>
2021-05-20 15:35:45 +0600

    first commit

---
+++ src/main/java/letscode/sarafan/controller/MessageController.java
@@ -0,0 +1,55 @@
+package letscode.sarafan.controller;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import letscode.sarafan.domain.Message;
+import letscode.sarafan.domain.Views;
+import letscode.sarafan.repo.MessageRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    private final MessageRepository messageeRepository;
+
+    @Autowired
+    public MessageController(MessageRepository messageeRepository) {
+        this.messageeRepository = messageeRepository;
+    }
+
+    @GetMapping
+    @JsonView(Views.IdName.class)
+    public List<Message> list() {
+        return messageeRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    @JsonView(Views.FullMessage.class)
+    public Message getOne(@PathVariable("id") Message message) {
+        return message;
+    }
+
+    @PostMapping
+    public Message create(@RequestBody Message message) {
+        message.setCreationDate(LocalDateTime.now());
+        return messageeRepository.save(message);
+    }
+
+    @PutMapping("{id}")
+    public Message update(
+            @PathVariable("id") Message messageFromDb,
+            @RequestBody Message message
+    ) {
+        BeanUtils.copyProperties(message, messageFromDb, "id");
+        return messageeRepository.save(messageFromDb);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Message message) {
+        messageeRepository.delete(message);
+    }
+}
---
+++ src/main/java/letscode/sarafan/domain/Views.java
@@ -0,0 +1,7 @@
+package letscode.sarafan.domain;
+
+public class Views {
+    public interface Id{}
+    public interface IdName extends Id{}
+    public interface FullMessage extends Id{}
+}
